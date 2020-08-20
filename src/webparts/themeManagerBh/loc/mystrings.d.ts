e99ad6ef0cd35f2e43795577a314d7d7ab5a295d
bijubasheer <bijubasheer@users.noreply.github.com>
2020-08-20 09:51:41 -0400

    First Commit

---
+++ src/webparts/themeManagerBh/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IThemeManagerBhWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ThemeManagerBhWebPartStrings' {
+  const strings: IThemeManagerBhWebPartStrings;
+  export = strings;
+}
